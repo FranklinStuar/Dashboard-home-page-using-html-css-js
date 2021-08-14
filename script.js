8db0a437dccc1fcc3d0957c55f7e7d1b8a0c0ccb
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2021-08-14 18:08:37 -0500

    Dashboard home page using html+css+js

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    let toggle = document.querySelector(".toggle")
+    let navigation = document.querySelector(".navigation")
+    toggle.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
+
